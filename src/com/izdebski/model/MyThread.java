ea72c040cf13e711beda3461f29e7fbaf3505101
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-05-15 18:28:16 +0300

    Lambda Expression part 1

---
+++ src/com/izdebski/model/MyThread.java
@@ -0,0 +1,9 @@
+package com.izdebski.model;
+
+public class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("My is thread executing...");
+    }
+}
